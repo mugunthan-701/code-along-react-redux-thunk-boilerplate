525f7169207146948bb36f0f688af17a51d2872a
mugunthan s <mugunthan701@gmail.com>
2024-01-16 21:14:03 +0530

    comp

--- redux-thunk/src/Component/action.js
+++ redux-thunk/src/Component/action.js
@@ -1,19 +1,11 @@
 import {fetchData} from "./actionType";
 import axios from "axios";
 
-
-
 export const dataFetching = () => (dispatch)=>{
         axios.get("https://jsonplaceholder.typicode.com/users")
         .then((res)=>dispatch(fetchDatafunction(res.data)))
         .catch((err)=>console.log(err))
       }
-
-
-
-
-
-
 export const fetchDatafunction = (payload) =>{
     
     return {type:fetchData , payload}
